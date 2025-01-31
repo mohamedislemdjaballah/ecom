e4a7e9989319e3e3a6ebbe65d85d70626c8d8fde
mohamedislemdjaballah <med.islam07@hotmail.com>
2025-01-31 23:11:11 +0100

    add scribe and crud functions in warehouse controller

--- server/app/Http/Controllers/WareHouseController.php
+++ server/app/Http/Controllers/WareHouseController.php
@@ -3,8 +3,46 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use \Illuminate\Http\JsonResponse;
 class WareHouseController extends Controller
 {
-    //
+    public function index(): JsonResponse
+    {
+        // Validate the request...
+        
+        // Return all warehouses
+        return response()->json(['message' => 'All warehouses']);
+    }
+
+    public function store(Request $request): JsonResponse
+    {
+        // Validate the request...
+        
+        // Create a new warehouse
+        return response()->json(['message' => 'Warehouse created']);
+    }
+
+    public function show($id): JsonResponse
+    {
+        // Validate the request...
+        
+        // Return the warehouse with the given id
+        return response()->json(['message' => 'Warehouse with id ' . $id]);
+    }
+
+    public function update(Request $request, $id): JsonResponse
+    {
+        // Validate the request...
+        
+        // Update the warehouse with the given id
+        return response()->json(['message' => 'Warehouse with id ' . $id . ' updated']);
+    }
+
+    public function destroy($id): JsonResponse
+    {
+        // Validate the request...
+        
+        // Delete the warehouse with the given id
+        return response()->json(['message' => 'Warehouse with id ' . $id . ' deleted']);
+    }
 }
--- server/routes/api.php
+++ server/routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\WareHouseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::prefix('v1')->group(function () {
+    Route::apiResource('warehouses', WareHouseController::class);
+    Route::apiResource('products', ProductController::class);
+    
+});
